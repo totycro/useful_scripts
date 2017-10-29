a39c754a39515ee33e75f0f83f5dc8a8a9f50c0d
Bernhard Mallinger <b.mallinger@gmx.at>
2017-10-29 15:51:02 +0100

    kodi-cmd.sh: Add play_file and play_youtube

--- kodi-cmd.sh
+++ kodi-cmd.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+
+# settings
 KODI="10.0.0.10:80"
+NFS_SERVER="10.0.0.35"
+
+
+# this produces sane behavior for "main $@"
+IFS=$(echo $IFS | tr -d ' ')
 
 
 function debug () {
@@ -25,6 +32,16 @@ function get_player_id () {
   curl -v --header "Content-Type: application/json" --data-binary '{"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}' http://10.0.0.10/jsonrpc | jq '.result[0].playerid'
 }
 
+
+function arg_or_clipboard_content () {
+  if [ -z "$1" ] ; then
+    xclip -o
+  else
+    echo $1
+  fi
+}
+
+
 function main () {
   case $1 in 
     toggle)
@@ -35,6 +52,25 @@ function main () {
       ;;
     prev)
       execute_cmd "Player.GoTo" '{"playerid": '$(get_player_id)', "to": "previous"}'
+      ;;
+    play_file)
+      file=$(arg_or_clipboard_content $2)
+      path="$(pwd)/${file}"
+      nfs_path="nfs://${NFS_SERVER}${path}"
+      execute_cmd "Player.Open" "{\"item\": {\"file\": \"${nfs_path}\"}}"
+      ;;
+    play_youtube)
+      youtube_url=$(arg_or_clipboard_content $2)
+      regex="^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*"
+      if [[ "$youtube_url" =~ $regex ]]; then
+        id=${BASH_REMATCH[7]}
+      else
+        error "Failed to parse youtube url"
+        exit 1
+      fi
+      debug "Playing video with id ${id}"
+      youtube_plugin_path="plugin://plugin.video.youtube/?action=play_video&videoid=$id"
+      execute_cmd "Player.Open" "{\"item\": {\"file\": \"${youtube_plugin_path}\"}}"
       ;;
     vol)
       case $2 in
