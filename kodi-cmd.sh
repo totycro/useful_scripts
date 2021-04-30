188283210e5d57f165d217acd60b23afa203c299
Bernhard Mallinger <bernhard.mallinger@gmx.at>
2021-04-30 19:21:02 +0200

    Update to kodi 19 api

--- kodi-cmd.sh
+++ kodi-cmd.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # settings
-KODI="192.168.0.164:80"
+
+set -x
+KODI_IP=`jq --raw-output ".ip" < /home/osmc/cli_remote.conf`
+KODI_PORT=`jq --raw-output ".port" < /home/osmc/cli_remote.conf`
+KODI="${KODI_IP}:${KODI_PORT}"
 NFS_SERVER=$(ip a s wlp5s0 | grep "inet " | awk '{ print $2 }' | cut -d'/' -f 1 | head -n 1 )
 
 
@@ -22,35 +26,13 @@ function execute_cmd () {
   action=$1
   params=$2
   debug "Executing $action with $params"
-  curl --silent --show-error --header "Content-Type: application/json" --data-binary '{"jsonrpc": "2.0", "method": "'"$action"'", "params": '"$params"',  "id": 1}' http://${KODI}/jsonrpc
+  curl -v -u kodi:kodi --show-error --header "Content-Type: application/json" --data-binary '{"jsonrpc": "2.0", "method": "'"$action"'", "params": '"$params"',  "id": 1}' http://${KODI}/jsonrpc
 }
 
 
 function get_player_id () {
   # returns first player id, which seems to be the only one in my usage
-  curl -s --header "Content-Type: application/json" --data-binary '{"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}' http://${KODI}/jsonrpc | jq '.result[0].playerid'
-}
-
-
-function get_seek_time() {
-  seek_in_seconds=$1
-  player_id=$(get_player_id)
-  current_time=$(curl -s --header "Content-Type: application/json" --data-binary '{"jsonrpc": "2.0", "method": "Player.GetProperties",  "params": {"properties": ["time"], "playerid": '$player_id'}, "id": 1}' http://${KODI}/jsonrpc | jq '.result.time')
-  python3 - <<END_OF_PYTHON
-# yeah, not gonna do this in bash
-import json
-import datetime
-data = $current_time
-# this won't work with videos longer than 24 hours
-current_time = datetime.datetime(1, 1, 1, data['hours'], data['minutes'], data['seconds'], data['milliseconds'] * 1000)
-seek_time = current_time + datetime.timedelta(seconds=$seek_in_seconds)
-print({
-  "hours": seek_time.hour,
-  "minutes": seek_time.minute,
-  "seconds": seek_time.second,
-  "milliseconds": int(seek_time.microsecond / 1000),
-})
-END_OF_PYTHON
+  curl -s -u kodi:kodi --header "Content-Type: application/json" --data-binary '{"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}' http://${KODI}/jsonrpc | jq '.result[0].playerid'
 }
 
 
@@ -84,19 +66,12 @@ function main () {
       execute_cmd "Player.GoTo" '{"playerid": '$(get_player_id)', "to": "previous"}'
       ;;
     seek)
-      case $2 in
-        +)
-          seek_time=$(get_seek_time $3 | tr \' \")
-          ;;
-        -)
-          seek_time=$(get_seek_time -$3 | tr \' \")
-          ;;
-        *)
-          error "$2 is neither \"+\" nor \"-\""
-          exit 1
-          ;;
-      esac
-      execute_cmd "Player.Seek" "{\"value\": $seek_time, \"playerid\": $(get_player_id)}"
+      if [ ${2} = "-" ] ; then
+          SIGN="-"
+      else
+          SIGN=""
+      fi
+      execute_cmd "Player.Seek" "{\"value\": {\"seconds\": ${SIGN}${3}}, \"playerid\": $(get_player_id)}"
       ;;
     play_file)
       file=$(arg_or_clipboard_content "$arg1")
