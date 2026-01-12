b740e3545fe915027784a857b2f4c1c74a80dc69
Bernhard Mallinger <b.mallinger@gmx.at>
2026-01-12 15:32:16 +0100

    Use espeak-ng for battry check, it works with modern audio setups

--- check_battery.sh
+++ check_battery.sh
@@ -4,6 +4,8 @@ set -eux -o pipefail
 
 BATTERY_REMAINING=$(acpi --battery | grep -E -o '[0-9]+%' | tr -d '%')
 
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
 if [ "${BATTERY_REMAINING}" -le 20 ] ; then
-    echo "la batería está agotándose, quedan ${BATTERY_REMAINING}%" | espeak -v es-la
+    echo "la batería está agotándose, quedan ${BATTERY_REMAINING}%" | espeak-ng -v es-la
 fi
